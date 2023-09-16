4ace7a19e458b0790814a8605ea8f9079626c497
Marc Terradas <terradas.marc@gmail.com>
2023-09-16 09:14:00 +0200

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./application/**/*.{js,ts,jsx,tsx}",
+    "./infrastructure/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
