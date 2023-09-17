420d5095f9c0b625cef23cae080c22cfa82149e4
Marc Terradas <terradas.marc@gmail.com>
2023-09-17 17:18:18 +0200

    import logo

--- application/layout/Navbar.js
+++ application/layout/Navbar.js
@@ -1,11 +1,13 @@
 import { router } from "@/infrastructure/functions/router";
 import LanguageDropDown from "@/application/navbar/LanguageDropDown";
+import Logo from "@/application/navbar/Logo";
 
 export default function Navbar() {
   const { locale: selectedLanguageCode, locales: languagesCodes } = router();
 
   return (
-    <nav className="flex items-center justify-center w-full h-16 sm:justify-end">
+    <nav className="flex items-center justify-between w-full h-16">
+      <Logo />
       <LanguageDropDown
         selectedLanguageCode={selectedLanguageCode}
         languagesCodes={languagesCodes}
