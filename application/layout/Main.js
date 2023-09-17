03876839685bdb2a775d62a6a3d4d10d1cb84362
Marc Terradas <terradas.marc@gmail.com>
2023-09-17 18:43:47 +0200

    add subtitle

--- application/layout/Main.js
+++ application/layout/Main.js
@@ -1,9 +1,11 @@
 import Title from "@/application/main/Title";
+import SubTitle from "@/application/main/SubTitle";
 
 export default function Main() {
   return (
-    <main className="flex flex-wrap items-center justify-center">
+    <main className="flex flex-wrap items-center justify-center mt-24">
       <Title />
+      <SubTitle />
     </main>
   );
 }
